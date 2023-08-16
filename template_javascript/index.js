6e34cc0ffd94c42af60664431665be5e4bc0445c
Aires-Leo <leonardo_aires@live.com>
2023-08-16 19:49:48 -0300

    Atualização

---
+++ template_javascript/index.js
@@ -0,0 +1,31 @@
+// Insto é um comentário em uma linha//
+/* Esse comando permite que seja incluído 
+um comentário mais extenso, de mais de uma 
+linha */
+
+//console.log ("Olá Mundo")//
+
+//prompt("qual o seu nome?")//
+
+/*const idade = 23;
+
+idade = 24; 
+
+let maca = 24;
+
+maca = 25;*/
+
+/*const nomeCompleto = "Leonardo"
+
+const idade = 32*/
+
+console.log ("Olá!" , "Meu nome é", nome, "e eu tenho", idade , "anos")
+
+const primeiroNome = "Leonardo"; 
+const sobrenome = "Aires da Silva"
+const idade = 32
+const estudante = true;
+
+console.log (primeiroNome, sobrenome, idade, estudante)
+
+
